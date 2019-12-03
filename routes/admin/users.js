d3c137d40395b7aa1b598c0970b93266f4df01ae
tuyennv@vnpt.vn <Abc432@#$>
2019-12-03 15:42:28 +0700

    fix router

--- routes/admin/users.js
+++ routes/admin/users.js
@@ -2,17 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 // Load User model
-const User = require('../../models/User');
 const userCopntroller = require('../../controllers/userController');
 const { isLoggedIn, notLoggedIn } = require('../../configs/auth');
 
 router.get('/index', isLoggedIn, userCopntroller.index);
 
 //login
-router.get('/', notLoggedIn, userCopntroller.login);
-
-// Login
-router.post('/', notLoggedIn, userCopntroller.postLogin);
+router.get('/', notLoggedIn, userCopntroller.login)
+.post('/', notLoggedIn, userCopntroller.postLogin);
 
 // router.get('/dang-ky', notLoggedIn, userCopntroller.register);
 
@@ -22,22 +19,21 @@ router.post('/', notLoggedIn, userCopntroller.postLogin);
 // Logout
 router.get('/logout', userCopntroller.logout);
 
-router.get('/profile', isLoggedIn, userCopntroller.profile);
+router.get('/profile', isLoggedIn, userCopntroller.profile)
+.post('/profile', isLoggedIn, userCopntroller.postProfile);
 
-router.post('/profile', isLoggedIn, userCopntroller.postProfile);
+router.get('/changpass', isLoggedIn, userCopntroller.changpass)
+.post('/changpass', isLoggedIn, userCopntroller.postChangpass);
 
-router.get('/changpass', isLoggedIn, userCopntroller.changpass);
-router.post('/changpass', isLoggedIn, userCopntroller.postChangpass);
+router.get('/user/list/:page', isLoggedIn, userCopntroller.listUsers)
+.post('/user/list/:page', isLoggedIn, userCopntroller.postListUsers);
 
-router.get('/user/list/:page', isLoggedIn, userCopntroller.listUsers);
-router.post('/user/list/:page', isLoggedIn, userCopntroller.postListUsers);
-
-router.get('/user/edit/:id', isLoggedIn, userCopntroller.editUser);
-router.post('/user/edit/:id', isLoggedIn, userCopntroller.postEditUser);
+router.get('/user/edit/:id', isLoggedIn, userCopntroller.editUser)
+.post('/user/edit/:id', isLoggedIn, userCopntroller.postEditUser);
 
 router.get('/user/delete/:id', isLoggedIn, userCopntroller.deleteUser);
 
-router.get('/user/add', isLoggedIn, userCopntroller.addUser);
-router.post('/user/add', isLoggedIn, userCopntroller.postAddUser);
+router.get('/user/add', isLoggedIn, userCopntroller.addUser)
+.post('/user/add', isLoggedIn, userCopntroller.postAddUser);
 
 module.exports = router;
