e83c7666ec3ee9c5ed3253cf7b0bf343ca602b0a
tuyennv@vnpt.vn <Abc432@#$>
2020-08-05 16:33:58 +0700

    u

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -9,7 +9,7 @@ router.get('/index/:page', isLoggedIn, checkAccess, productContronller.index)
 router.get('/add', isLoggedIn, checkAccess, productContronller.add)
     .post('/add', isLoggedIn, checkAccess, productContronller.addPost);
 router.get('/edit/:id', isLoggedIn, checkAccess, productContronller.edit)
-    .post('/edit/:id', isLoggedIncheckAccess,, productContronller.editPost);
+    .post('/edit/:id', isLoggedIn, checkAccess, productContronller.editPost);
 router.post('/search', isLoggedIn, checkAccess, productContronller.search);
 router.get('/delete/:id', isLoggedIn, checkAccess, productContronller.delete);
 
